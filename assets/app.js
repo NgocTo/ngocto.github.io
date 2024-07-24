9ea330618a44f04464a742664048fceb5e19250f
NgocTo <baongoc1308@gmail.com>
2024-07-24 02:26:55 -0400

    homepage mobile

--- assets/app.js
+++ assets/app.js
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#E4A5AF",
         "opacity": 0.4,
         "width": 1
       },
--- assets/scripts.js
+++ assets/scripts.js
@@ -2,6 +2,18 @@ setTimeout(()=> {
   typeWriter("Hello World ", "#intro-text", 0);
 }, 500)
 
+// Set progress value (0 to 1)
+function setProgress(value) {
+  let progressBars = document.querySelectorAll('.progress');
+  progressBars.forEach(progressBar => {
+    if (progressBar.checkVisibility())
+      progressBar.style.setProperty('--progress', progressBar.getAttribute('data-efficiency'));
+  });
+}
+
+// Example: Set progress to 75%
+
+setProgress(0.75);
 
 function typeWriter(text, container, i) {
   if (i < text.length) {
